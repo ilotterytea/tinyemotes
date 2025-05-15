f1e2ef2ef8f8523d2626d76b1656dee3f27669f0
ilotterytea <me@ilotterytea.kz>
2025-05-16 03:42:41 +0500

    upd: 0.2.2 -> 0.3.0

--- src/version.php
+++ src/version.php
@@ -1,7 +1,7 @@
 <?php
 // please leave it as it is ;)
 define("TINYEMOTES_NAME", "TinyEmotes");
-define("TINYEMOTES_VERSION", "0.2.2");
+define("TINYEMOTES_VERSION", "0.3.0");
 define("TINYEMOTES_LINK", "https://github.com/ilotterytea/tinyemotes");
 
 if ($s = file_get_contents("../.git/refs/heads/master")) {
